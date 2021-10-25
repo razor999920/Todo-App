36d0999f95d94ee70193b5a3593c91f932d25a70
razor <raza9999ali@gmail.com>
2021-10-25 17:24:17 -0400

    managing header component

--- src/components/UI/Header.js
+++ src/components/UI/Header.js
@@ -1,9 +1,40 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = (props) => {
   return (
     <React.Fragment>
-      Header
+      <header>
+        <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+          <div href="http://www.google.ca" className="navbar-brand">
+            <a>Razor</a>
+          </div>
+          <ul className="navbar-nav">
+            <li>
+              <Link className="nav-link" to="/home/razor">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link className="nav-link" to="/todos">
+                Todos
+              </Link>
+            </li>
+          </ul>
+          <ul className="navbar-nav navbar-collapse justify-content-end">
+            <li>
+              <Link className="nav-link" to="/login">
+                Login
+              </Link>
+            </li>
+            <li>
+              <Link className="nav-link" to="/logout">
+                Logout
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
       <hr />
     </React.Fragment>
   );
